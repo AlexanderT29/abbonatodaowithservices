0451cee5506805b968229c8fba038ecfeb6b3a5a
AlexanderT <95099967+AlexanderT29@users.noreply.github.com>
2026-03-08 21:11:44 +0100

    Update Constants.java
    Tolto il valore della password dal file 'Constants'

--- src/it/prova/dao/Constants.java
+++ src/it/prova/dao/Constants.java
@@ -2,6 +2,6 @@ package it.prova.dao;
 
 public interface Constants {
     public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-    public static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/abbonatodaowithservices?user=root&password=Peppa290396_&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+    public static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/abbonatodaowithservices?user=root&password=???&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
 
 }
